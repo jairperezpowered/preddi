03c40a932a0e2a50badf49071e675027f4d7c12e
Jair Pérez <jairperezpowered@gmail.com>
2018-11-12 19:53:00 -0500

    add react

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,28 @@
+const webpack = require('webpack');
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.jsx',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
     output: {
       path: __dirname + '/out',
       publicPath: '/',
       filename: 'bundle.js'
     },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ],
     devServer: {
-      contentBase: './out'
+      contentBase: './out',
+      hot: true
     }
-  };
+  }
